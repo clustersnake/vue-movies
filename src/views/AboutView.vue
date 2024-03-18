740b944384d88924f2de78cd01b66a585ca73639
Eloy Ramirez <clustersnake45@gmail.com>
2024-03-18 16:09:06 -0400

    refactor: Remove unused content from about view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,37 +1,7 @@
-<script setup lang="ts">
-import useMovieService from '@/composables/services/useMovieService'
-import { ref, onMounted } from 'vue'
-const { getMovies } = useMovieService()
-const data = ref<any>({})
-const movies = ref<any[]>([])
-
-// const load = async () => {
-//   movies.value = await getMovies()
-// }
-
-// load()
-
-onMounted(async () => {
-  data.value = await getMovies()
-  movies.value = data.value.results
-  console.log(data.value)
-})
-</script>
+<script setup lang="ts"></script>
 <template>
   <div class="about">
     <h1>This is an about page!</h1>
-    <pre>
-  {{ movies }}
-  </pre
-    >
-    <section>
-      <article v-for="movie in movies" :key="movie.id">
-        <img :src="movie.poster_path" :alt="movie.title" />
-        <p>
-          {{ movie.title }}
-        </p>
-      </article>
-    </section>
   </div>
 </template>
 <style>
