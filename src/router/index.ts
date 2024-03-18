f5c122b5d83853a0b88a8699f55d1635a6d9b55d
Eloy Ramirez <clustersnake45@gmail.com>
2024-03-18 16:12:26 -0400

    feat: Create new routes
    - Create login view
    - Install vee-validate to handle form validation
    - Update routes and add meta field to handle authentication

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import {useSessionStore} from '@/stores';
 import HomeView from '../views/HomeView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,12 +9,18 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        requiresAuth: true
+      },
     },
     {
       path: '/movies/:id',
       name: 'movieDetails',
-      component: () => import('../views/MovieView.vue')
+      component: () => import('../views/MovieView.vue'),
+      meta: {
+        requiresAuth: true
+      },
     },
     {
       path: '/about',
@@ -20,9 +28,38 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/AboutView.vue'),
+      meta: {
+        requiresAuth: true
+      },
+    },
+    {
+      path: '/login',
+      name: 'login',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/LoginView.vue'),
+      meta: {
+        requiresAuth: false
+      },
     }
   ]
+})
+
+router.beforeEach((to) => {
+  console.log(to);
+  const sessionStore = useSessionStore();
+
+  if(to.meta.requiresAuth && !sessionStore.userData.authenticated){
+    console.log('not authenticated');
+ return {
+      path: '/login',
+      // save the location we were at to come back later
+      query: { redirect: to.fullPath },
+    }
+
+  }
 })
 
 export default router
---
+++ src/views/LoginView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { Form, Field } from 'vee-validate'
+import {useSessionStore} from '@/stores';
+
+const sessionStore = useSessionStore();
+//ecode...
+const onSubmit = (values: any) => {
+  console.log(JSON.stringify(values, null, 2))
+  sessionStore.login(values.email);
+
+}
+</script>
+
+<template>
+  <h1>Login vue</h1>
+  <div class="text-black">
+    <Form @submit="onSubmit">
+      <Field type="email" name="email" />
+      <button>Log in</button>
+    </Form>
+  </div>
+</template>
+
+<style scoped>
+/* code... */
+</style>
