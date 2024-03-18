c05bffaae7784fb2653a8f26b48bba15b245f646
Eloy Ramirez <clustersnake45@gmail.com>
2024-03-18 16:01:21 -0400

    feat: Create store index file
    - Create index file to export store composables
    - Add userdata to store
    - Create login and logout functions in store

--- src/stores/session.ts
+++ src/stores/session.ts
@@ -1,8 +1,26 @@
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 import { defineStore } from 'pinia'
+import {useRoute, useRouter} from 'vue-router';
 
 export const useSessionStore = defineStore('session', () => {
   const genre = ref('')
+  const userData = reactive({
+    email: '',
+    authenticated: false
+  })
+
+  const route = useRoute();
+  const router = useRouter();
+  const login = (email: string) => {
+
+    userData.email = email
+    userData.authenticated = true
+    router.push(route.query.redirect as string || '/');
+  }
+  const logout = () => {
+    userData.email = '';
+    userData.authenticated = false;
+  }
 
   // const currentGenre = computed(():string => genre.value)
 
@@ -10,5 +28,7 @@ export const useSessionStore = defineStore('session', () => {
     genre.value = id
   }
 
-  return { genre, selectGenre }
+  return { genre, selectGenre, userData, login , logout}
 })
+
+export default useSessionStore;
