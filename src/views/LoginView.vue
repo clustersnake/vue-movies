0e79d23a3c768657e7ad910f546f2c84065b354d
Eloy Ramirez <clustersnake45@gmail.com>
2024-03-19 00:23:44 -0400

    feat: Update login form

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -12,13 +12,14 @@ const onSubmit = (values: any) => {
 </script>
 
 <template>
-  <h1>Login vue</h1>
-  <div class="text-black">
-    <Form @submit="onSubmit">
+  <article class="card border-2 py-10 px-8 rounded-xl">
+    <header><h2 class="text-center mb-8">The Movies!</h2></header>
+    <Form @submit="onSubmit" class="flex flex-col gap-3 items-start">
       <Field type="email" name="email" />
-      <button>Log in</button>
+      <Field type="password" name="password" />
+      <button class="border-2 rounded-md px-2 py-1 bg-green-800 hover:bg-green-200 hover:text-green-800 hover:border-green-800">Login</button>
     </Form>
-  </div>
+  </article>
 </template>
 
 <style scoped>
