4f5e84bec574530ca192b2268e578cccd69e8d88
Eloy Ramirez <clustersnake45@gmail.com>
2024-03-19 00:34:36 -0400

    refactor: Adjust header styles

--- src/components/base/TheHeader.vue
+++ src/components/base/TheHeader.vue
@@ -4,13 +4,16 @@ import { RouterLink } from 'vue-router'
 
 <template>
   <section
-    class="w-full border-b border-gray-500 flex justify-center py-2 md:mb-4 lg:mb-6 xl:mb-8 mb-2"
+    class="w-full border-b border-gray-500 py-2 md:mb-4 lg:mb-6 xl:mb-8 mb-2"
   >
-    <nav class="container flex items-center">
+    <nav class="container flex mx-auto justify-between">
       <RouterLink to="/">
         <img alt="logo" class="logo" src="@/assets/logo.svg" width="32" height="32" />
       </RouterLink>
-      <h3 class="text-center w-full">The Movies!</h3>
+      <h3 class="">The Movies!</h3>
+      <RouterLink to="/">
+      Logout
+      </RouterLink>
     </nav>
   </section>
 </template>
