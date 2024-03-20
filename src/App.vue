f1929634492fd8a2c7e91d969c33a38fdcd825bc
Eloy Ramirez <clustersnake45@gmail.com>
2024-03-19 22:54:26 -0400

    fix: Remove class prop from layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
 import { RouterView, } from 'vue-router'
-// import layouts from '@/layouts';
 import {useSessionStore} from '@/stores';
 const store = useSessionStore();
 
 </script>
 
 <template>
-  <component :is="store.layout || 'div'" class="">
+  <component :is="store.layout || 'div'">
     <router-view />
   </component>
 </template>
